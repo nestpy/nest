0179f409268870c7063599acb63868260e6e3fab
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-08 22:26:05 +0100

    feat: :building_construction: add abstractmethod in INestApplication

--- nest/common/interfaces/nest_application_interface.py
+++ nest/common/interfaces/nest_application_interface.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
-
+from nest.common.enums import VersioningType
+from typing import List
 
 class INestApplication(ABC):
+
+    @abstractmethod
+    def enableVersioning(self, type: VersioningType, defaultVersioning: str = "1"):
+        pass
+
     @abstractmethod
     def listen(self, host: str = "0.0.0.0", port: int = 3000) -> None:
+        pass
+
+    @abstractmethod
+    def setGlobalPrefix(self, prefix: str, exclude: List[str] = []) -> None:
         pass
