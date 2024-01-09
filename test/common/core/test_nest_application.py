f40c1d00b61346437037020aca807e8a3c41e3f1
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-09 22:40:32 +0100

    test: :test_tube: add more examples feature to develop in cors

--- test/common/core/test_nest_application.py
+++ test/common/core/test_nest_application.py
@@ -17,9 +17,9 @@ class TestNestApplication:
         assert app.config.globalPrefix == False
         assert app.config.versioning == False
 
-    def test_ShouldConfigOptions_WhenInstanceWithCorsTrue(self):
+    def test_ShouldDefaultConfigOptions_WhenInstanceWithCorsTrue(self):
         @Module()
-        class app_module: 
+        class app_module:
             pass
 
         config_expect = CorsOptions()
@@ -27,6 +27,23 @@ class TestNestApplication:
         config = ApplicationConfig(cors=True)
         app = NestApplication(app_module, config)
 
-        print(app.config.cors)
+        assert config_expect.__eq__(app.config.cors)
 
-        assert config_expect.__eq__(app.config.cors) 
+
+    def test_ShouldException_WhenInstanceWithCorsNotBool(self): pass
+    def test_ShouldException_WhenUseWildcardWithMoreValues(self): pass
+    def test_ShouldException_WhenNotUseRequestMethodString(self): pass
+
+    def test_ShouldConfigOptions_WhenUseEnableCorsMethod(self):
+        @Module()
+        class app_module:
+            pass
+
+        config_expect = CorsOptions(credentials=True)
+
+        config = ApplicationConfig()
+        app = NestApplication(app_module, config)
+
+        app.enableCors(config_expect)
+
+        assert config_expect.__eq__(app.config.cors)
