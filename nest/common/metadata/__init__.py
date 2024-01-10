0d19fb04d29ff7b68d0b44b11f175f17a49f2ef6
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-10 22:32:21 +0100

    feat: :sparkles: add docsoptions metadata

--- nest/common/metadata/__init__.py
+++ nest/common/metadata/__init__.py
@@ -1,4 +1,5 @@
 from .cors_options import CorsOptions as CorsOptions
+from .docs_options import DocsOptions as DocsOptions
 from .global_prefix_options import GlobalPrefixOptions as GlobalPrefixOptions
 from .route_metadata import Route as Route
 from .versioning_options import VersioningOptions as VersioningOptions
---
+++ nest/common/metadata/docs_options.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+
+from nest.common.enums import DocsType
+
+from typing import Dict, List
+
+
+class Contact(BaseModel):
+    name: str
+    url: str
+    email: str
+
+class LicenseInfo(BaseModel):
+    name: str
+    identifier: str
+
+class ExternalDocs(BaseModel):
+    description: str
+    url: str
+
+class OpenapiTags(BaseModel):
+    name: str
+    description: str
+    externalDocs: ExternalDocs = {}
+
+class DocsOptions(BaseModel):
+    type: DocsType = DocsType.NONE
+    swagger_url: str = '/docs'
+    redoc_url: str = '/redoc'
+    title: str = 'Nestpy API Documentation'
+    description: str = ''
+    summary: str = ''
+    version: str = '0.1.0'
+    terms_of_service: str = ''
+    contact: Contact = { }
+    license_info: LicenseInfo = { }
+    openapi_tags: List[OpenapiTags] = [ ]
+    openapi_url: str = '/openapi.json'
+
