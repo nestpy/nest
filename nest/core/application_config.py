6ece8acda66c401f081ffc040224a117548b186d
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-10 22:32:43 +0100

    feat: :sparkles: add docs params in applicationconfig

--- nest/core/application_config.py
+++ nest/core/application_config.py
@@ -1,14 +1,21 @@
-from nest.common.metadata import CorsOptions, GlobalPrefixOptions, VersioningOptions
+from nest.common.metadata import (
+    CorsOptions,
+    DocsOptions,
+    GlobalPrefixOptions,
+    VersioningOptions
+)
 
 
 class ApplicationConfig():
     def __init__(
         self,
         cors: bool = False,
+        docs: bool = False,
         globalPrefix: bool = False,
         versioning: bool = False
     ):
         self.cors: bool | CorsOptions = cors
+        self.docs: bool | DocsOptions = docs
         self.globalPrefix: bool | GlobalPrefixOptions = globalPrefix
         self.versioning: bool | VersioningOptions = versioning
 
