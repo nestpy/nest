6551ecaf7cf4f546f823edb75e9c52ebe9090110
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-25 23:19:32 +0100

    test: :white_check_mark: add new test for request method

--- test/common/decorators/http/test_request_method_decorator.py
+++ test/common/decorators/http/test_request_method_decorator.py
@@ -6,55 +6,44 @@ import pytest
 
 class TestRequestMethod:
 
-    @pytest.mark.parametrize("path,route_expect,method", [
-        ('/', RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
-        ('/', RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
-        ('/api', RouteArgs(path='/api', methods=['DELETE'], status_code=200), Delete),
-        ('/', RouteArgs(path='/', methods=['GET'], status_code=200), Get),
-        ('/', RouteArgs(path='/', methods=['GET'], status_code=200), Get),
-        ('/api', RouteArgs(path='/api', methods=['GET'], status_code=200), Get),
-        ('/', RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
-        ('/', RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
-        ('/api', RouteArgs(path='/api', methods=['HEAD'], status_code=200), Head),
-        ('/', RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
-        ('/', RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
-        ('/api', RouteArgs(path='/api', methods=['OPTIONS'], status_code=200), Options),
-        ('/', RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
-        ('/', RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
-        ('/api', RouteArgs(path='/api', methods=['PATH'], status_code=200), Path),
-        ('/', RouteArgs(path='/', methods=['POST'], status_code=201), Post),
-        ('/', RouteArgs(path='/', methods=['POST'], status_code=201), Post),
-        ('/api', RouteArgs(path='/api', methods=['POST'], status_code=201), Post),
-        ('/', RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
-        ('/', RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
-        ('/api', RouteArgs(path='/api', methods=['PUT'], status_code=200), Put),
+    @pytest.mark.parametrize("route_expect,method", [
+        (RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
+        (RouteArgs(path='/', methods=['GET'], status_code=200), Get),
+        (RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
+        (RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
+        (RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
+        (RouteArgs(path='/', methods=['POST'], status_code=201), Post),
+        (RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
     ])
-    def test_GetDecorator_AssignsRouteArgsToFunction_UsingPathParam(self, path, route_expect, method):
+    def test_RequestMethodDecorators_AssignsRouteArgsToFunction_UsingDefaultParam(self, route_expect, method):
 
-        @method(path)
+        @method()
         def fn(): pass
 
         assert hasattr(fn, ROUTER_WATERMARK)
         assert getattr(fn, ROUTER_WATERMARK)
-
         assert hasattr(fn, ROUTE_INFO_KEY)
         assert route_expect.__eq__(getattr(fn, ROUTE_INFO_KEY))
 
-    def test_GetDecorator_AssignsRouteArgsToFunction_UsingDefaultParam(self):
+    @pytest.mark.parametrize("path,route_expect,method", [
+        ('/', RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
+        ('/api', RouteArgs(path='/api', methods=['DELETE'], status_code=200), Delete),
+        ('/', RouteArgs(path='/', methods=['GET'], status_code=200), Get),
+        ('/api', RouteArgs(path='/api', methods=['GET'], status_code=200), Get),
+        ('/', RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
+        ('/api', RouteArgs(path='/api', methods=['HEAD'], status_code=200), Head),
+        ('/', RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
+        ('/api', RouteArgs(path='/api', methods=['OPTIONS'], status_code=200), Options),
+        ('/', RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
+        ('/api', RouteArgs(path='/api', methods=['PATH'], status_code=200), Path),
+        ('/', RouteArgs(path='/', methods=['POST'], status_code=201), Post),
+        ('/api', RouteArgs(path='/api', methods=['POST'], status_code=201), Post),
+        ('/', RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
+        ('/api', RouteArgs(path='/api', methods=['PUT'], status_code=200), Put),
+    ])
+    def test_RequestMethodDecorators_AssignsRouteArgsToFunction_UsingPathParam(self, path, route_expect, method):
 
-        user_expect = RouteArgs(
-            path='/',
-            methods=['GET'],
-            status_code=200
-        )
-
+        @method(path)
         def fn(): pass
 
-        get_decorator = Get()
-        fn_decorated = get_decorator(fn)
-
-        assert hasattr(fn_decorated, ROUTER_WATERMARK)
-        assert getattr(fn_decorated, ROUTER_WATERMARK)
-
-        assert hasattr(fn_decorated, ROUTE_INFO_KEY)
-        assert user_expect.__eq__(getattr(fn_decorated, ROUTE_INFO_KEY))
+        assert route_expect.__eq__(getattr(fn, ROUTE_INFO_KEY))
