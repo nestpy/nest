3168426f053d87d4e466eb04cfda012d2cc16a7e
Manuel S. Lemos <manuelslemos@gmail.com>
2024-01-25 23:09:05 +0100

    test: :white_check_mark: add new test for request method

--- test/common/decorators/http/test_request_method_decorator.py
+++ test/common/decorators/http/test_request_method_decorator.py
@@ -1,4 +1,4 @@
-from nest.common import Get, Post
+from nest.common import Delete, Get, Head, Options, Path, Post, Put
 from nest.common.keys import ROUTER_WATERMARK, ROUTE_INFO_KEY
 from nest.common.metadata import RouteArgs
 
@@ -7,13 +7,27 @@ import pytest
 class TestRequestMethod:
 
     @pytest.mark.parametrize("path,route_expect,method", [
+        ('/', RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
+        ('/', RouteArgs(path='/', methods=['DELETE'], status_code=200), Delete),
+        ('/api', RouteArgs(path='/api', methods=['DELETE'], status_code=200), Delete),
         ('/', RouteArgs(path='/', methods=['GET'], status_code=200), Get),
         ('/', RouteArgs(path='/', methods=['GET'], status_code=200), Get),
         ('/api', RouteArgs(path='/api', methods=['GET'], status_code=200), Get),
+        ('/', RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
+        ('/', RouteArgs(path='/', methods=['HEAD'], status_code=200), Head),
+        ('/api', RouteArgs(path='/api', methods=['HEAD'], status_code=200), Head),
+        ('/', RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
+        ('/', RouteArgs(path='/', methods=['OPTIONS'], status_code=200), Options),
+        ('/api', RouteArgs(path='/api', methods=['OPTIONS'], status_code=200), Options),
+        ('/', RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
+        ('/', RouteArgs(path='/', methods=['PATH'], status_code=200), Path),
+        ('/api', RouteArgs(path='/api', methods=['PATH'], status_code=200), Path),
         ('/', RouteArgs(path='/', methods=['POST'], status_code=201), Post),
-        ('/', RouteArgs(path='/', methods=['POST'], status_code=201
-                        ), Post),
-        ('/api', RouteArgs(path='/api', methods=['POST'], status_code=201), Post)
+        ('/', RouteArgs(path='/', methods=['POST'], status_code=201), Post),
+        ('/api', RouteArgs(path='/api', methods=['POST'], status_code=201), Post),
+        ('/', RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
+        ('/', RouteArgs(path='/', methods=['PUT'], status_code=200), Put),
+        ('/api', RouteArgs(path='/api', methods=['PUT'], status_code=200), Put),
     ])
     def test_GetDecorator_AssignsRouteArgsToFunction_UsingPathParam(self, path, route_expect, method):
 
